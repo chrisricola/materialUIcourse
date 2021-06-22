2b55fb1b1589da24e6b025b0d8ba16c801796700
Chris Rico <chris.r.rico@gmail.com>
2021-06-22 11:47:59 -0500

    added toolbar responsiveness, links and style

--- materialuicourse/src/components/App.js
+++ materialuicourse/src/components/App.js
@@ -1,14 +1,27 @@
 /* eslint-disable no-use-before-define */
 import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import theme from './ui/Theme';
 import Header from './ui/Header';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      Yerrr
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={() => <div>home</div>} />
+          <Route exact path="/services" component={() => <div>Services</div>} />
+          <Route exact path="/customsoftware" component={() => <div>Custom Software</div>} />
+          <Route exact path="/mobileapps" component={() => <div>Mobile Apps</div>} />
+          <Route exact path="/websites" component={() => <div>Websites</div>} />
+          <Route exact path="/revolution" component={() => <div>The Revolution</div>} />
+          <Route exact path="/about" component={() => <div>About Us</div>} />
+          <Route exact path="/contact" component={() => <div>Contact Us</div>} />
+          <Route exact path="/estimate" component={() => <div>Estimate</div>} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
