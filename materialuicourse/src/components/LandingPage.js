a556f8285a38180cfe239b2f008e2b02acfe92d1
Chris Rico <chris.r.rico@gmail.com>
2021-07-19 20:37:51 -0700

    added call to action pageand responsive

--- materialuicourse/src/components/LandingPage.js
+++ materialuicourse/src/components/LandingPage.js
@@ -9,6 +9,8 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 
+import CallToAction from "../components/ui/CallToAction";
+
 import animationData from "../animations/landinganimation/data";
 import customSoftwareIcon from "../assets/Custom Software Icon.svg";
 import mobileAppsIcon from '../assets/mobileIcon.svg';
@@ -295,6 +297,10 @@ export default function LandingPage() {
                     </Grid>
                     <div className={classes.infoBackground} />
                 </Grid>
+            </Grid>
+            <Grid item>
+                {/*-----Call to Action Block-----*/}
+                <CallToAction></CallToAction>
             </Grid>
         </Grid >
     );
