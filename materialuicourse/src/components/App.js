7739eb37cdd55ababa0d1e9f1a73aea4d0a28bf0
Chris Rico <chris.r.rico@gmail.com>
2021-07-28 19:36:37 -0700

    added the Custom Software page, responsive, no links

--- materialuicourse/src/components/App.js
+++ materialuicourse/src/components/App.js
@@ -7,6 +7,8 @@ import Header from './ui/Header';
 import Footer from "./ui/Footer";
 import LandingPage from './LandingPage';
 import Services from './Services';
+import CustomSoftware from './CustomSoftware';
+
 
 function App() {
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -19,7 +21,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={(props) => <LandingPage {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
           <Route exact path="/services" render={(props) => <Services {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
-          <Route exact path="/customsoftware" component={() => <div>Custom Software</div>} />
+          <Route exact path="/customsoftware" render={(props) => <CustomSoftware {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
           <Route exact path="/mobileapps" component={() => <div>Mobile Apps</div>} />
           <Route exact path="/websites" component={() => <div>Websites</div>} />
           <Route exact path="/revolution" component={() => <div>The Revolution</div>} />
